fb4a3678d7b0a3b219a02fb6dc47b2ad2d8bb3d5
Silen Resras <mugafelhida98@gmail.com>
2024-10-05 04:45:44 +0300

    second commit

--- src/components/store/authStore.js
+++ src/components/store/authStore.js
@@ -1,8 +1,8 @@
 import { create } from "zustand";
 import axios from 'axios';
 
-//const API_URL = import.meta.env.MODE === "development" ? "http://localhost:7000/api/auth" : "/api/auth";
-const API_URL = "http://localhost:7000/api/auth"
+const API_URL = import.meta.env.MODE === "development" ? "http://localhost:7000/api/auth" : "/api/auth";
+
 
 
 axios.defaults.withCredentials = true;
